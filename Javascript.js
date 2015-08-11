da8f1ad86b1cf3722ea760e394b36fb8a4523de5
bivilfd <bivil@freshdesk.com>
2015-08-11 12:35:19 +0530

    HTML page added
    more from js

--- Javascript.js
+++ Javascript.js
@@ -1,1 +1,28 @@
+
 confirm("The alert on load to confirm!")
+
+var promp = prompt("Your Name? ")
+console.log(promp)
+
+//number, string, boolean
+
+console.log(2*5)
+console.log("Hello")
+
+console.log("Xiao Hui".length < 122);
+console.log("Goody Donaldson".length !== 8);
+console.log(8*2 === 16);
+
+
+if (2%1)
+{
+	console.log("The True case")
+}
+else
+{
+	console.log("Batman".substring(0,3))
+	console.log("Laptop".substring(3,6))
+	console.log("Janumary".substring(0,3))
+	console.log("Melbourne is great".substring(0,12))
+	console.log("Hamburgers".substring(3,10))
+}
