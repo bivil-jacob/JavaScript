6fdc2bdf3cc6c19523bb8c910ea39bab96aa8de2
bivilfd <bivil@freshdesk.com>
2015-08-24 13:11:09 +0530

    Dragon Slayer game

--- Javascript.js
+++ Javascript.js
@@ -134,6 +134,35 @@ else
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var DragonSlayer = function()
+{
+	var slaying = true;
+
+var youHit = Math.floor(Math.random() *2 );
+var damageThisRound = Math.floor(Math.random() * 5 + 1);
+var totalDamage = 0;
+
+while (slaying) {
+  if (youHit) {
+    console.log("You hit the dragon and did " + damageThisRound + " damage!");
+    totalDamage += damageThisRound;
+    
+    if (totalDamage >= 4) {
+      console.log("You did it! You slew the dragon!");
+      slaying = false;
+    } else {
+      youHit = Math.floor(Math.random() * 2);
+    }
+  } else {
+    console.log("The dragon burninates you! You're toast.");
+    slaying = false;
+  }
+}
+
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var RockPaperScissors = function(){
 
@@ -196,8 +225,8 @@ for (var i = 1; i < 11; i = i + 1)
 
 
 //RockPaperScissors();
-stringSearch();
-
+//stringSearch();
+DragonSlayer();
 //sorangeCost(5)
 
 //bieber()
